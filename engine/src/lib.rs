722998a8aae528922683cf9201d344c8442f0340
Kammy <kammy@leafia.eu>
2025-08-03 09:21:33 +0200

    Update content CDN + add KME

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -10,7 +10,7 @@ pub use crate::features::modal::*;
 pub use crate::features::navigation::*;
 
 pub const METADATA_CDN_ORIGIN: &str = "https://cdn.music.leafia.eu";
-pub const CONTENT_CDN_ORIGIN: &str = "https://cryptv1.static.leafia.eu";
+pub const CONTENT_CDN_ORIGIN: &str = "https://cryptv1.music.leafia.eu";
 
 #[wasm_bindgen(start)]
 pub async fn start() {
